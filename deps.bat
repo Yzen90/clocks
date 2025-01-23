7f5cfbb899c16c16807f21be786131fa4a805d5e
Edgar Montiel Cruz <edgar@yzen.pro>
2025-01-23 16:24:19 -0500

    Updated deps.bat

--- deps.bat
+++ deps.bat
@@ -2,3 +2,6 @@
 git submodule init
 git pull --recurse-submodules
 vcpkg install --no-print-usage
+npm install
+yq --version > nul 2>&1 || winget install MikeFarah.yq
+clang --version > nul 2>&1 || winget install LLVM.LLVM
